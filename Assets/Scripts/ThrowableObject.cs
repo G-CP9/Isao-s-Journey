e8424e8a4519d1e7bfa97dc74001a90247a3d850
Lucas <37332932+LucasFrS@users.noreply.github.com>
2022-11-06 23:52:15 +0100

    add scene and mockup objects, add ThrowableObject prefab and script

---
+++ Assets/Scripts/ThrowableObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableObject : MonoBehaviour
+{
+    public float dirY;
+    public float strength;
+    
+    public void Throw()
+    {
+        
+    }
+}
