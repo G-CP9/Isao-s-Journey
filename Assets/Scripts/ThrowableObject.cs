7edee00231773f2484ad4d0bb2c63982065dc44a
Lucas <37332932+LucasFrS@users.noreply.github.com>
2022-11-07 20:04:00 +0100

    Add throwableobject functionality

--- Assets/Scripts/ThrowableObject.cs
+++ Assets/Scripts/ThrowableObject.cs
@@ -4,11 +4,42 @@ using UnityEngine;
 
 public class ThrowableObject : MonoBehaviour
 {
-    public float dirY;
-    public float strength;
-    
-    public void Throw()
+    [SerializeField] float _InitialVelocity;
+    [SerializeField] float _Angle;
+    [SerializeField] Transform _FirePoint;
+
+    private void Start() {
+        transform.position = _FirePoint.position;
+    }
+
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float angle = _Angle * Mathf.Deg2Rad;
+            StopAllCoroutines();
+            StartCoroutine(Coroutine_Movement(_InitialVelocity, angle));
+        }
+    }
+
+    IEnumerator Coroutine_Movement(float initialVelocity, float angle)
+    {
+        float time = 0;
+        while (time < 100)
+        {
+            float x = initialVelocity * time * Mathf.Cos(angle);
+            float y = initialVelocity * time * Mathf.Sin(angle) - (1f / 2f) * - Physics.gravity.y * Mathf.Pow(time, 2);
+            transform.position = _FirePoint.position + new Vector3(x, y, 0);
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Ground")
+        {
+            StopAllCoroutines();
+        }
     }
 }
