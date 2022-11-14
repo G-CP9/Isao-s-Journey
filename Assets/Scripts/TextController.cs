d63552f70efe814dcc735243922ea3d14e69fecc
xXYuiKXx <al405529@uji.es>
2022-11-14 11:52:15 +0100

    Start

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -12,18 +12,33 @@ public class TextController : MonoBehaviour
     //Texto Para escoger flor a guardar
     public GameObject FlorGuardar;
     public GameObject FlorTirar;
+
+    //Start game
+    public GameObject StartWindow;
+
+
     void Start()
     {
         box = FindObjectOfType<BoxController>();
         bin = FindObjectOfType<BinController>();
+        PauseGame();
 
 
     }
 
+
+
+
+
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            StartWindow.SetActive(false);
+            ResumeGame();
 
+        }
         if(box.box_open)
         {
             FlorGuardar.SetActive(true);
@@ -42,6 +57,16 @@ public class TextController : MonoBehaviour
             FlorTirar.SetActive(false);
         }
 
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
     }
 
 
