726de9fbbbaabd916e291ebe4ef4466e6e8c3c93
xXYuiKXx <al405529@uji.es>
2022-12-31 12:18:15 +0100

    aaaaaaaaaaaaaaaaa

--- Assets/Scripts/Cooking/Food_controller.cs
+++ Assets/Scripts/Cooking/Food_controller.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class Food_controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public void Spawn()
-    {
+    Vector2 difference = Vector2.zero;
+    Collider2D object_collider;
+    Vector2 originalPos;
 
+    // Start is called before the first frame update
+    private void Start()
+    {
+        object_collider = GetComponent<Collider2D>();
+        originalPos = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
     }
+
+    void Food_added()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
 }
